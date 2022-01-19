dd31be34bb384efe578bc3e0bf90fab45631e0c7
ZeroGoYoosee <qz777zp@live.cn>
2022-01-19 17:19:15 +0800

    feat: dice 1

--- lib/games/dices/dice.dart
+++ lib/games/dices/dice.dart
@@ -23,10 +23,55 @@ class DiceWidget extends StatefulWidget {
 class _DiceWidgetState extends State<DiceWidget> {
   @override
   Widget build(BuildContext context) => Container(
-    decoration: BoxDecoration(
-            color: Theme.of(context).colorScheme.onBackground,
-            border: Border.all(width: widget.borderSize),
-            borderRadius: BorderRadius.all(Radius.circular(widget.radius)),
-          ),
-  );
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.secondary,
+          border: Border.all(width: widget.borderSize, color: Theme.of(context).colorScheme.secondary),
+          borderRadius: BorderRadius.all(Radius.circular(widget.radius)),
+        ),
+        child: CustomPaint(
+          size: const Size(48.0, 48.0),
+          painter: _DicePainter(
+              backgroundColor: Theme.of(context).colorScheme.secondary,
+              diceColor: Theme.of(context).colorScheme.background,
+              count: 1),
+        ),
+      );
+}
+
+class _DicePainter extends CustomPainter {
+  var count = 1;
+  Color backgroundColor;
+  Color diceColor;
+
+  _DicePainter(
+      {required this.count,
+      required this.backgroundColor,
+      required this.diceColor})
+      : super();
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var dicePainter = Paint()
+      ..color = diceColor
+      ..strokeWidth = 0.0
+      ..isAntiAlias = true;
+    var bgPainter = Paint()
+      ..color = backgroundColor
+      ..strokeWidth = 0.0
+      ..isAntiAlias = true;
+    switch (count) {
+      case 1:
+        {
+          // canvas.drawRect(Rect.fromLTRB(0.0, 0.0, size.width, size.height), bgPainter);
+          canvas.drawCircle(Offset(size.width / 2, size.height / 2), size.height / 4, dicePainter);
+        }
+        break;
+      default:
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
 }
